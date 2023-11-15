a12e83278b7e8d9a044de80052130c856cb720cb
Narendran <narendran.chellappa@southcoastscience.com>
2023-11-15 14:04:24 +0000

    Updated

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def has_duplicate_letters(sentence):
     return False
 
 
+add_fun = add
 sentence1 = "This is the third change"
 sentence2 = "Hello change 3 "
 print(has_duplicate_letters(sentence1))
 print(has_duplicate_letters(sentence2))
-print(add(2, 3, 4, 5, 6))
+print(add_fun(2, 3, 4, 5, 6))
 print(sub(2, 3))
 print(mul(10, 2, 2))
