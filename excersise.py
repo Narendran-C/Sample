61d5715916a864b2a85f818e19f07a1acdc946fe
Narendran <narendran.chellappa@southcoastscience.com>
2023-11-20 12:42:57 +0000

    Updated

--- excersise.py
+++ excersise.py
@@ -7,9 +7,9 @@ a = list(x)                                    # tuple converted to list
 b = list(y)                                    # set converted to list
 print('\n')
 
-print('Covert to list from tuple : ', a,   type(a))
-print('Covert to list from set   : ', b,   type(b))
-print('Actual list defined       : ', z,   type(z))
-print('Array elements            : ', arr, type(arr))
+print('Converted from tuple to list : ', a,   type(a))
+print('Converted from set to list   : ', b,   type(b))
+print('Actual list defined          : ', z,   type(z))
+print('Array elements               : ', arr, type(arr))
 
 print('')
