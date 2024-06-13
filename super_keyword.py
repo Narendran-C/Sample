f4659a64a04ef2afb34577a8f0913e2126c104fc
Narendran <narendran.chellappa@southcoastscience.com>
2024-06-13 15:31:11 +0100

    Super Keyword

---
+++ super_keyword.py
@@ -0,0 +1,27 @@
+class A:
+    def __init__(self):
+        print("Class A Constructor")
+
+    def display(self):
+        print("You are in Class A")
+
+
+class A1():
+    def __init__(self):
+        super().__init__()
+        print("Class A1 Constructor")
+
+    def display(self):
+        print("You are in Class A1")
+
+
+class B(A1, A):
+    def __init__(self):
+        super().__init__()
+        print("Class B Constructor")
+
+    def display(self):
+        print("You are in Class B")
+
+
+obj2 = B()
